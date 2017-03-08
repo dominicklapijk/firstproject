b1852063442e1a7d0c7112f2167c6d7b9addc2e0
dominic <dominic.klapwijk@gmail.com>
2017-03-08 19:55:07 +0100

    message

---
+++ jquerybonanza/jquerybonanza.js
@@ -0,0 +1,27 @@
+
+    //  $(document).ready(function(){
+    //   $(".button").click(function(){
+    //     alert("pressed`button");
+    // });
+    //   });
+
+     $(document).ready(function(){
+      // $("#fade").click(function(){
+      //   alert("pressed`button");
+    	
+    	$("#append").click(function(){
+        $("#append").append("<b>increase</b>");
+    	});
+      
+      $("#colour").click(function(){
+        $("#pcolour").css("color","red");
+    	});
+      
+      $("#fade").click(function(){
+       $("#pfades").fadeOut();
+    	});
+      
+    	});
+      	
+ 	
+      
